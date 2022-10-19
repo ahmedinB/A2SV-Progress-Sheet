30573b4008830a5d3c8e9678d6e46213ea925899
ahmedinB <62029069+ahmedinB@users.noreply.github.com>
2022-10-19 22:06:58 +0400

    Create maxSubArray.py

---
+++ maxSubArray.py
@@ -0,0 +1,22 @@
+class Solution(object):
+    def maxSubArray(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: int
+        """
+        maxsum = float('-inf')
+        n = len(nums)
+        currentsum = 0
+        start = 0
+        end = 0
+        prefixsum = 0
+        while end < n:
+            currentsum = currentsum + nums[end]
+            prefixsum += nums[end]
+            maxsum = max(maxsum, currentsum)
+            if prefixsum < 0:
+                start = end
+                prefixsum = 0
+                currentsum = 0
+            end += 1
+        return maxsum
