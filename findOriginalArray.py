adaf29c06152b0c83d2f0491250578838b1ae219
ahmedinB <62029069+ahmedinB@users.noreply.github.com>
2022-11-08 15:47:16 +0400

    Update findOriginalArray.py

--- findOriginalArray.py
+++ findOriginalArray.py
@@ -4,30 +4,19 @@ class Solution(object):
         :type changed: List[int]
         :rtype: List[int]
         """
+        n = len(changed)
+        ctr = Counter(changed)
+        res = []
         changed.sort()
-        mid= len(changed)//2
-        l1=[]
-        l2=[]
-        zeros=0
-        for i in changed:
-            if i==0:
-                zeros+=1
-                changed.remove(0)
-            elif i*2 in changed:
-
-##              a problem with duplicated zeros how can you delete a duplicated item
-                l1.append(i)
-                l2.append(i*2)
-                changed.remove(i*2)
-        for z in range(0,zeros,2):
-            l1.append(0)
-            l2.append(0)
-        if zeros%2!=0:
-            return
-        elif len(l1)!=mid or len(l1)==0:
-            print("case", l1)
-            return
-        return changed
+        for el in changed:
+            if el != 0 and ctr[el] > 0 and ctr[2 * el] > 0:
+                res.append(el)
+                ctr[el] -= 1
+                ctr[2 * el] -= 1
+        res.extend([0] * (ctr[0] // 2))
+        if 2 * len(res) != n:
+            return []
+        return res
 
 S=Solution()
 print(S.findOriginalArray([0,0,0,0]))
